d64dc3e4a9abc8d59f37734e2594ee3c9af66f85
vinaykumar229-ai <vinay5f8@sasi.ac.in>
2025-06-20 12:42:25 +0530

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
-};
+}
 
 export default MyApp;
