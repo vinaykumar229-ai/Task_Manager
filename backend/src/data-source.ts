7fa5490c867caa968860bad483c75a02cc4b1546
vinaykumar229-ai <vinay5f8@sasi.ac.in>
2025-06-20 09:44:07 +0530

    Update data-source.ts

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,9 +1,14 @@
 import { DataSource } from 'typeorm';
 import { Task } from './entity/Task';
+import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
-  type: 'sqlite',
-  database: 'db.sqlite',
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: true,
   logging: false,
   entities: [Task],
